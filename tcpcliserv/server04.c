de564755ce8f0294f19c56dfa582db713b04d869
mingle <32973902@qq.com>
2017-10-24 16:09:25 +0800

    Not

--- tcpcliserv/server04.c
+++ tcpcliserv/server04.c
@@ -12,10 +12,20 @@ void str_echo(int sockfd) {
 	}
 }
 
+void sig_chld(int signo) {
+	pid_t pid;
+	int stat;
+
+	// pid = wait(&stat);
+	while ((pid = waitpid(-1, &stat, WNOHANG)) > 0) {
+		printf("child %d terminated\n", pid);
+	}
+}
+
 int main(int argc, char const *argv[]) {
 	int listenfd, connfd;
 	pid_t childpid;
-	socklen_t clilen;
+	socklen_t chilen;
 	struct sockaddr_in cliaddr, servaddr;
 
 	listenfd = Socket(AF_INET, SOCK_STREAM, 0);
@@ -24,20 +34,31 @@ int main(int argc, char const *argv[]) {
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 	servaddr.sin_port = htons(SERV_PORT);
+
 	Bind(listenfd, (SA *)&servaddr, sizeof(servaddr));
 
 	Listen(listenfd, LISTENQ);
 
-	for(;;) {
-		clilen = sizeof(cliaddr);
-		connfd = Accept(listenfd, (SA *)&cliaddr, &clilen);
+	Signal(SIGCHLD, sig_chld);
+
+	for (;;) {
+		chilen = sizeof(cliaddr);
+		if ((connfd = accept(listenfd, (SA *)&cliaddr, &chilen)) < 0) {
+			if (errno == EINTR) {
+				printf("einter\n");
+				continue;
+			} else {
+				err_sys("accept error");
+			}
+		} 
 		if ((childpid = Fork()) == 0) {
-			Close(listenfd);
-			str_echo(connfd);
+				Close(listenfd);
+				str_echo(connfd);
+				Close(connfd);
+				exit(0);
+			}
 			Close(connfd);
-			exit(0);
-		}
-		Close(connfd);
 	}
+
 	return 0;
 }
