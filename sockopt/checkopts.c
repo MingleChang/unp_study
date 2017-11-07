f69df8f470bbd2593aaafda2594d8bc0af0aa05e
mingle <32973902@qq.com>
2017-11-07 16:54:05 +0800

    Not

--- sockopt/checkopts.c
+++ sockopt/checkopts.c
@@ -43,44 +43,9 @@ struct sock_opts{
 	"SO_USELOOPBACK",	SOL_SOCKET,		SO_USELOOPBACK,	sock_str_flag,
 	"IP_TOS",			IPPROTO_IP,		IP_TOS,			sock_str_int,
 	"IP_TTL",			IPPROTO_IP,		IP_TTL,			sock_str_int,
-#ifdef IPV6_DONTFRAG
-	"IPV6_DONTFRAG",	IPPROTO_IPV6,	IPV6_DONTFRAG,	sock_str_flag,
-#else
-	"IPV6_DONTFRAG",	0,				0,				NULL,
-#endif
-#ifdef IPV6_UNICAST_HOPS
-	"IPV6_UNICAST_HOPS",IPPROTO_IPV6,	IPV6_UNICAST_HOPS,sock_str_int,
-#else
-	"IPV6_UNICAST_HOPS",0,				0,				NULL,
-#endif
-#ifdef IPV6_V6ONLY
-	"IPV6_V6ONLY",		IPPROTO_IPV6,	IPV6_V6ONLY,	sock_str_flag,
-#else
-	"IPV6_V6ONLY",		0,				0,				NULL,
-#endif
+
 	"TCP_MAXSEG",		IPPROTO_TCP,	TCP_MAXSEG,		sock_str_int,
-	"TCP_NODELAY",		IPPROTO_TCP,	TCP_NODELAY,	sock_str_flag,
-#ifdef SCTP_AUTOCLOSE
-	"SCTP_AUTOCLOSE",	IPPROTO_SCTP,	SCTP_AUTOCLOSE,	sock_str_int,
-#else
-	"SCTP_AUTOCLOSE",	0,				0,				NULL,
-#endif
-#ifdef SCTP_MAXBURST
-	"SCTP_MAXBURST",	IPPROTO_SCTP,	SCTP_MAXBURST,	sock_str_int,
-#else
-	"SCTP_MAXBURST",	0,				0,				NULL,
-#endif
-#ifdef SCTP_MAXSEG
-	"SCTP_MAXSEG",		IPPROTO_SCTP,	SCTP_MAXSEG,	sock_str_int,
-#else
-	"SCTP_MAXSEG",		0,				0,				NULL,
-#endif
-#ifdef SCTP_NODELAY
-	"SCTP_NODELAY",		IPPROTO_SCTP,	SCTP_NODELAY,	sock_str_flag,
-#else
-	"SCTP_NODELAY",		0,				0,				NULL,
-#endif
-	NULL,				0,				0,				NULL
+	"TCP_NODELAY",		IPPROTO_TCP,	TCP_NODELAY,	sock_str_flag
 };
 
 int main(int argc, char const *argv[]) {
