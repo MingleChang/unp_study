5e89952073c9eed7aadbbe568237fce677222f88
Mingle <mingle890805@gmail.com>
2018-01-15 17:19:46 +0800

    not

---
+++ libroute/if_nameindex.c
@@ -0,0 +1,45 @@
+#include "unpifi.h"
+#include "unproute.h"
+struct if_nameindex *if_nameindex(void) {
+  char *buf, *next, *lim;
+  size_t len;
+  struct if_msghdr *ifm;
+  struct sockaddr *sa, *rti_info[RTAX_MAX];
+  struct sockaddr_dl *sdl;
+  struct if_nameindex *result, *ifptr;
+  char *namptr;
+
+  if ((buf = net_rt_iflist(0, 0, &len)) == NULL) {
+    return NULL;
+  }
+
+  if ((result = malloc(len)) == NULL) {
+    return NULL;
+  }
+  ifptr = result;
+  namptr = (char *)result + len;
+
+  lim = buf + len;
+  for (next = buf; next < lim, next += ifm->ifm_msglen) {
+    ifm = (struct if_msghdr *)next;
+    if (ifm->ifm_type == RTM_IFINFO) {
+      sa = (struct sockaddr *)(ifm + 1);
+      get_rtaddrs(ifm->ifm_addrs, sa, rti_info);
+      if ((sa = rti_info[RTAX_IFP]) != NULL) {
+        if (sa->sa_family == AF_LINK) {
+          sdl = (struct sockaddr_dl *)sa;
+          namptr -= sdl->sdl_nlen +1;
+          strncpy(namptr, &sdl->sdl_data[0], sdl->sdl_nlen);
+          namptr[sdl->sdl_nlen] = 0;
+          ifptr->if_name;
+          ifptr->if_index = sdl->sdl_index;
+          ifptr++;
+        }
+      }
+    }
+  }
+  ifptr->if_name = NULL;
+  ifptr->if_inex = 0;
+  free(buf);
+  return result;
+}
---
+++ route/checkudpsum.c
@@ -0,0 +1,17 @@
+#include "unp.h"
+#include <netinet/udp.h>
+#include <netinet/ip_var.h>
+#include <netinet/udp_var.h>
+
+int main(int argc, char const *argv[]) {
+  int mib[4], val;
+  size_t len;
+  mib[0] = CTL_NET;
+  mib[1] = AF_INET;
+  mib[2] = IPPROTO_UDP;
+  mib[3] = UDPCTL_CHECKSUM;
+  len = sizeof(val);
+  Sysctl(mib, 4, &val, &len, NULL, 0);
+  printf("udp checksum flag: %d\n", val);
+  return 0;
+}
