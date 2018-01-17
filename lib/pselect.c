da4eea18e1d945ef6c28d8922342914c79c5b758
Mingle <mingle890805@gmail.com>
2018-01-17 11:05:15 +0800

    not

---
+++ lib/pselect.c
@@ -0,0 +1,18 @@
+#include "unp.h"
+
+int pselect(int nfds, fd_set *rset, fd *wset, fd_set *xset, const struct timespec *ts, const sigset_t *sigmask) {
+  int n;
+  struct timeval tv;
+  sigset_t savemask;
+
+  if (ts != NULL) {
+    tv.tv_sec = ts->tv_sec;
+    tv.tv_usec = ts->tv_nsec / 1000;
+  }
+
+  sigprocmask(SIG_SETMASK, sigmask, &savemask);
+  n = select(nfds, rset, wset, xset, (ts == NULL) ? NULL : &tv);
+  sigprocmask(SIG_SETMASK, &savemask, NULL);
+
+  return n;
+}
