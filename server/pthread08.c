8c2c6a9469a3da5754d501b9dcd5336c9e84fe48
Mingle <mingle890805@gmail.com>
2018-02-26 11:44:07 +0800

    not

--- server/pthread08.c
+++ server/pthread08.c
@@ -6,3 +6,25 @@ void thread_make(int i) {
   Pthread_create(&tptr[i].thread_tid, NULL, &thread_main, (void *)i);
   return;
 }
+
+void *thread_main(void *arg) {
+  int connfd;
+  void web_child(int);
+
+  printf("thread %d starting\n", (int)arg);
+  for (;;) {
+    Pthread_mutex_lock(&clifd_mutex);
+    while (iget == iput) {
+      Pthread_cond_wait(&clifd_cond, &clifd_mutex);
+    }
+    connfd = clifd[iget];
+    if (++iget == MAXNCLI) {
+      iget = 0;
+    }
+    Pthread_mutex_unlock(&clifd_mutex);
+    tptr((int)arg).thread_count++;
+
+    web_child(connfd);
+    Close(connfd);
+  }
+}
